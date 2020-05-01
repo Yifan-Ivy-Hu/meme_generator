768abe5d96718d99f766854af3fb430e241d5d3b
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2020-05-01 16:17:14 -0400

    Implemented filtering photos according to users' input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import {SearchBox} from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      photos:[],
+      total:0,
+      searchField: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    // fetch recent photos if there is no input
+    fetch('https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=42d52d1f23ee4fe17369b0648d96a8cb&per_page=10&format=json&nojsoncallback=1')
+    .then(response =>response.json())
+    .then(data=> this.setState({photos:data.photos.photo, total:data.total}))
+  }
+
+  handleChange = e => {
+    // fetch photos according to the input
+    this.setState({searchField: e.target.value})
+    if (this.state.searchField) {
+      fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=42d52d1f23ee4fe17369b0648d96a8cb&text=${this.state.searchField}&per_page=50&format=json&nojsoncallback=1`)
+    .then(response =>response.json())
+    .then(data=> this.setState({photos:data.photos.photo, total:data.total}))
+    }
+  }
+
+  render() {
+    const {photos} = this.state;
+
+    return (
+      <div className="App">
+        <h1> Meme Generator</h1>
+        <SearchBox
+        placeholder='search images'
+        handleChange={this.handleChange}
+        />
+
+        <div className = "photos">
+        {photos.map((photo) => (
+          <div key={photo.id}>
+            <img alt='photo' src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} />
+          </div>
+        ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
